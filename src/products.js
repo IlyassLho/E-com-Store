b5f6deac3499a321bceaee3c639844ed14aa2488
IlyassLho <ilyasslhouari@gmail.com>
2025-12-14 19:20:51 +0100

    content: add product list with images and pricing details

---
+++ src/products.js
@@ -0,0 +1,179 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "منظم الحمام الجداري",
+    rating: 5,
+    reviewCount: 152,
+    oldPrice: "270 د.م",
+    price: "149 د.م",
+    image: "/images/3laqa-ta3-lhmam/img1.jpg",
+    description: [
+      "كتنظم ليك الدوش أو الطواليط",
+      "سهلة فالتركيب بلا حفير بلا والو (غي لصقة)",
+      "كتتحمل الوزن ومقاومة للماء",
+      "مصنوعة من البلاستيك عالي الجودة"
+    ],
+    gallery: [
+      "/images/3laqa-ta3-lhmam/img2.jpg",
+      "/images/3laqa-ta3-lhmam/img3.jpg",
+      "/images/3laqa-ta3-lhmam/img4.jpg",
+    ],
+    variants: [
+      {
+        id: 101,
+        name: "واحد بـ 149 درهم (الأكثر مبيعاً)",
+        price: "149 د.م",
+        qte: 1,
+      },
+      {
+        id: 102,
+        name: "جوج بـ 239 درهم (وفّر 60 درهم)",
+        price: "239 د.م",
+        qte: 2,
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "قنينة حرارية لحفظ الحرارة والبرودة",
+    rating: 5,
+    reviewCount: 156,
+    oldPrice: "279 د.م",
+    price: "139 د.م",
+    image: "/images/kas-ta3-lqhwa/img1.jpg",
+    description: [
+      "قنينة أنيقة تحافظ على سخونة أو برودة المشروبات لساعات طويلة",
+      "مصنوعة من الفولاذ المقاوم للصدأ بجودة عالية",
+      "غطاء يمكن استعماله ككأس للشرب",
+      "مانعة للتسرب وسهلة التنظيف",
+      "مثالية للسفر، العمل أو الرياضة"
+    ],
+    gallery: [
+      "/images/kas-ta3-lqhwa/img2.jpg",
+      "/images/kas-ta3-lqhwa/img3.jpg",
+      "/images/kas-ta3-lqhwa/img4.jpg"
+    ],
+    variants: [
+      {
+        id: 401,
+        name: "واحد بـ 139 درهم (الأكثر مبيعاً)",
+        price: "139 د.م",
+        qte: 1,
+      },
+      {
+        id: 402,
+        name: "جوج بـ 225 درهم (وفّر 55 درهم)",
+        price: "225 د.م",
+        qte: 2,
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: "مصباح مكتبي ذكي باللمس",
+    rating: 5,
+    reviewCount: 235,
+    oldPrice: "250 د.م",
+    price: "169 د.م",
+    image: "/images/lampa/img1.jpg",
+    description: [
+      "مصباح عصري بلمسة أنيقة يمكن تشغيله بلمسة واحدة",
+      "يتوفر على بطارية تدوم طويلاً",
+      "مقاوم للصدأ ومصنوع من مواد عالية الجودة",
+      "سطح مضاد للتغبيش يعطي إضاءة مريحة للعين",
+      "مثالي لغرفة النوم أو المكتب أو الدراسة"
+    ],
+    gallery: [
+      "/images/lampa/img2.jpg",
+      "/images/lampa/img3.jpg",
+      "/images/lampa/img4.jpg"
+    ],
+    variants: [
+      {
+        id: 501,
+        name: "واحد بـ 169 درهم (الأكثر مبيعاً)",
+        price: "169 د.م",
+        qte: 1,
+      },
+      {
+        id: 502,
+        name: "جوج بـ 269 درهم (وفّر 70 درهم)",
+        price: "269 د.م",
+        qte: 2,
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: "منظمة زاوية للحمام",
+    rating: 5,
+    reviewCount: 411,
+    oldPrice: "270 د.م",
+    price: "199 د.م",
+    image: "/images/3laqa-ta3-lhmam2/img1.jpg",
+    description: [
+      "كتعاونك تنظم جميع الأغراض ديالك فالدوش بشكل أنيق",
+      "كتتلصق بسهولة بلا ما تحتاج تحفر فالحائط",
+      "ستايل عصري باللون الأسود المطفي كيزيد لمسة فخمة وجمالية للحمّام",
+      "مقاوم للماء والصدأ، مصنوع من مواد عالية الجودة كيتحمّل الرطوبة",
+      "كتتحمل وزن ثقيل بلا ما تطيح",
+    ],
+    gallery: [
+      "/images/3laqa-ta3-lhmam2/img2.jpg",
+      "/images/3laqa-ta3-lhmam2/img3.jpg",
+      "/images/3laqa-ta3-lhmam2/img4.jpg",
+      "/images/3laqa-ta3-lhmam2/img5.jpg",
+      "/images/3laqa-ta3-lhmam2/img6.jpg"
+    ],
+    variants: [
+      {
+        id: 601,
+        name: "واحد بـ 199 درهم (الأكثر مبيعاً)",
+        price: "199 د.م",
+        qte: 1,
+      },
+      {
+        id: 602,
+        name: "جوج بـ 340 درهم (وفّر 50 درهم)",
+        price: "340 د.م",
+        qte: 2,
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: "معطر جو شمسي للسيارة",
+    rating: 5,
+    reviewCount: 173,
+    oldPrice: "200 د.م",
+    price: "139 د.م",
+    image: "/images/helicopter-parfum/img1.jpg",
+    description: [
+      "كتخدم بالطاقة الشمسية، المراوح كيدورو بوحدهم مع الضو ديال الشمس",
+      "كتعطي ريحة زوينة ومنعشة فالسيارة وكتبقى لمدة طويلة",
+      "كتجي مرفقة مع عطر خاص جاهز للاستعمال داخلها",
+      "تصميم أنيق وعصري على شكل هليكوبتر، كتزيد جمالية للطابلو ديال طوموبيلتك",
+      "مصنوعة من مواد عالية الجودة، مقاومة للحرارة",
+      "سهلة فالتركيب، كتجي مع لاصق قوي باش تثبتها"
+    ],
+    gallery: [
+      "/images/helicopter-parfum/img2.jpg",
+      "/images/helicopter-parfum/img3.jpg",
+      "/images/helicopter-parfum/img4.jpg"
+    ],
+    variants: [
+      {
+        id: 701,
+        name: "وحدة بـ 139 درهم (الأكثر مبيعاً)",
+        price: "139 د.م",
+        qte: 1,
+      },
+      {
+        id: 702,
+        name: "جوج بـ 219 درهم (وفّر 60 درهم)",
+        price: "219 د.م",
+        qte: 2,
+      }
+    ]
+  }
+];
